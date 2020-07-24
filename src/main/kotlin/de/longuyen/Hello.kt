ce456b614e2b9298a0ce1652fea73f9aebc66320
long nguyen <long.nguyen2306@protonmail.com>
2020-07-24 20:58:05 +0200

    Added data. Playing around with nd4j

--- src/main/kotlin/de/longuyen/Hello.kt
+++ src/main/kotlin/de/longuyen/Hello.kt
@@ -1,6 +1,24 @@
 package de.longuyen
 
-fun main(args: Array<String>) {
-    println("Hello, World")
+import org.nd4j.linalg.factory.Nd4j
+
+
+fun main() {
+
+    val x = Nd4j.zeros(3, 4)
+    println(x)
+
+    val ranks = x.rank()
+    println(ranks)
+
+    val shape = x.shape()
+    shape.forEach { print("$it ") }
+    println()
+
+    val length = x.length()
+    println(length)
+
+    val dt = x.dataType()
+    println(dt)
 }
 
