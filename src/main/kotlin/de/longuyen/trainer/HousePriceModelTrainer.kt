0992b309894d00faee4ec484e2bc740c06491d7a
long nguyen <long.nguyen2306@protonmail.com>
2020-07-26 09:11:47 +0200

    Working with nd4j later

--- src/main/kotlin/de/longuyen/trainer/HousePriceModelTrainer.kt
+++ src/main/kotlin/de/longuyen/trainer/HousePriceModelTrainer.kt
@@ -2,10 +2,19 @@ package de.longuyen.trainer
 
 import de.longuyen.core.Model
 import de.longuyen.core.NeuronalNetwork
+import de.longuyen.data.DataEncoder
+import de.longuyen.data.DataGenerator
 import de.longuyen.data.HousePriceDataEncoder
 import de.longuyen.data.HousePriceDataGenerator
+import org.nd4j.linalg.api.ndarray.INDArray
 
 class HousePriceModelTrainer {
+    private val dataGenerator: DataGenerator = HousePriceDataGenerator()
+    private val dataEncoder: DataEncoder
+
+    init {
+        dataEncoder = HousePriceDataEncoder(dataGenerator.getTrainingData())
+    }
     fun train() : Model{
         val dataGenerator = HousePriceDataGenerator()
         val trainingData = dataGenerator.getTrainingData()
@@ -17,8 +26,16 @@ class HousePriceModelTrainer {
         val model = NeuronalNetwork()
         return model
     }
-}
 
-fun main(){
+    fun getTrainingData() : Pair<INDArray, INDArray>{
+        return dataEncoder.encode()
+    }
 
+    fun getTestingData() : INDArray {
+        return dataEncoder.encodeFutureData(dataGenerator.getTestingData())
+    }
+
+    fun getValidatingData() : INDArray {
+        return dataEncoder.encodeFutureData(dataGenerator.getValidatingData())
+    }
 }
