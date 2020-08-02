560d2d920005ee9806a7d1be200fb339b004d323
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-02 19:59:43 +0200

    Fixed some validation

--- src/main/kotlin/de/longuyen/trainer/MnistModelTrainer.kt
+++ src/main/kotlin/de/longuyen/trainer/MnistModelTrainer.kt
@@ -40,7 +40,7 @@ class MnistModelTrainer :
         val x = testingData.first
         val y = testingData.second
 
-        val history = neuronalNetwork.train(X, Y, x, y, epochs = 200)
+        val history = neuronalNetwork.train(X, Y, x, y, epochs = 1000)
         val xData = DoubleArray(history["val-loss"]!!.size)
         for (i in 0 until history["val-loss"]!!.size) {
             xData[i] = i.toDouble()
